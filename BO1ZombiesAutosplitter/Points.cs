26538e136f387da65686396f823d955cd6c11699
Aldrik Ramaekers <aldrikboy@gmail.com>
2018-06-24 11:10:05 +0200

    ye

---
+++ BO1ZombiesAutosplitter/Points.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO1ZombiesAutosplitter
+{
+    public static class Points
+    {
+        public static void InitializePoints(ref resolution[] resolutions)
+        {
+            resolutions = new resolution[]
+            {
+                R_1600x900()
+            };
+        }
+
+        private static resolution R_1600x900()
+        {
+            resolution r = new resolution();
+            r.window_width = 1606;
+            r.window_height = 929;
+
+            r.levels = new level[] {
+                new level()
+                {
+                    lvl = 70,
+                    pointsToMatch = new point[]
+                    {
+                        // number seven
+                       new point(7,4),
+                       new point(20,4),
+                       new point(41,4),
+                       new point(40,11),
+                       new point(36,17),
+                       new point(29,29),
+                       new point(25,38),
+                       new point(21,47),
+                       new point(17,62),
+                       new point(16,77),
+
+                         // number zero
+                        new point(64,42),
+                        new point(100,10),
+                        new point(84,2),
+                        new point(68,36),
+                        new point(103,31),
+                        new point(104,45),
+                        new point(97,56),
+                        new point(101,65),
+                        new point(94,74),
+                        new point(70,70),
+                }
+            },
+
+                new level()
+                {
+                    lvl = 50,
+                    pointsToMatch = new point[]
+                    {
+                        // number five
+                       new point(41,3),
+                       new point(10,4),
+                       new point(26,3),
+                       new point(8,13),
+                       new point(8,26),
+                       new point(8,36),
+                       new point(27,29),
+                       new point(37,34),
+                       new point(42,42),
+                       new point(43,53),
+                       new point(40,65),
+
+                       new point(34,72),
+                       new point(25,75),
+                       new point(11,72),
+                       new point(7,64),
+
+                        // number zero
+                        new point(64,42),
+                        new point(100,10),
+                        new point(84,2),
+                        new point(68,36),
+                        new point(103,31),
+                        new point(104,45),
+                        new point(97,56),
+                        new point(101,65),
+                        new point(94,74),
+                        new point(70,70),
+                }
+            },
+
+                new level()
+                {
+                    lvl = 30,
+                    pointsToMatch = new point[]
+                    {
+                        // number three
+                        new point(8,14),
+                        new point(14,3),
+                        new point(40,10),
+                        new point(37,28),
+                        new point(22,36),
+                        new point(37,42),
+                        new point(43,55),
+                        new point(40,69),
+                        new point(27,74),
+                        new point(12,73),
+                        new point(7,64),
+
+                        // number zero
+                        new point(64,42),
+                        new point(100,10),
+                        new point(84,2),
+                        new point(68,36),
+                        new point(103,31),
+                        new point(104,45),
+                        new point(97,56),
+                        new point(101,65),
+                        new point(94,74),
+                        new point(70,70),
+                    }
+            },
+
+                new level()
+                {
+                    lvl = 15,
+                    pointsToMatch = new point[]
+                    {
+                        // number one
+                        new point(30,2),
+                        new point(7,14),
+                        new point(28,76),
+
+                        // number five
+                        new point(100,3),
+                        new point(84,76),
+                        new point(103,53),
+                        new point(86,28),
+                        new point(68,36),
+                    }
+            },
+
+                new level()
+                {
+                    lvl = 10,
+                    pointsToMatch = new point[]
+                {
+                        new point(98,22),
+                        new point(9,64),
+                        new point(145,60),
+                        new point(169,51),
+                        new point(192,41),
+                        new point(225,22),
+                        new point(135,64),
+
+                        new point(106,85, false),
+                        new point(76,17, false),
+                        new point(106,85, false),
+                        new point(28,79, false),
+                        new point(28,44, false),
+                        new point(50,12, false),
+                        new point(75,39, false),
+                        new point(80,84, false),
+                        new point(101,12, false),
+                        new point(237,62, false),
+                    }
+            },
+
+                new level()
+                {
+                    lvl = 5,
+                    pointsToMatch = new point[]
+                    {
+                        new point(98,22),
+                        new point(9,64),
+                        new point(145,60, false),
+                        new point(169,51, false),
+                        new point(192,41, false),
+
+                        new point(106,85, false),
+                        new point(76,17, false),
+                        new point(106,85, false),
+                        new point(28,79, false),
+                        new point(28,44, false),
+                        new point(50,12, false),
+                        new point(75,39, false),
+                        new point(80,84, false),
+                        new point(101,12, false),
+                    }
+                },
+
+                new level()
+                {
+                    lvl = 4,
+                    pointsToMatch = new point[]
+                    {
+                        new point(90,19),
+                        new point(93,87),
+                        new point(98,22, false),
+                        new point(9,64, false),
+
+                        new point(106,85, false),
+                        new point(76,17, false),
+                        new point(106,85, false),
+                        new point(28,79, false),
+                        new point(28,44, false),
+                        new point(50,12, false),
+                        new point(75,39, false),
+                        new point(80,84, false),
+                        new point(101,12, false),
+                    }
+                },
+
+                new level()
+                {
+                    lvl = 3,
+                    pointsToMatch = new point[]
+                    {
+                       new point(15,12),
+                       new point(37,15),
+                       new point(63,16),
+                       new point(93,88, false),
+                       new point(98,22, false),
+                       new point(9,64, false),
+
+                       new point(106,85, false),
+                       new point(76,17, false),
+                       new point(106,85, false),
+                       new point(28,79, false),
+                       new point(28,44, false),
+                       new point(50,12, false),
+                       new point(75,39, false),
+                       new point(80,84, false),
+                       new point(101,12, false),
+                    }
+            },
+
+                new level()
+                {
+                    lvl = 2,
+                    pointsToMatch = new point[]
+                    {
+                        new point(14,13),
+                        new point(36,14),
+                        new point(38,29),
+                        new point(63,16, false),
+                        new point(93,88, false),
+                        new point(98,22, false),
+                        new point(9,64, false),
+
+                        new point(106,85, false),
+                        new point(76,17, false),
+                        new point(106,85, false),
+                        new point(28,79, false),
+                        new point(28,44, false),
+                        new point(50,12, false),
+                        new point(75,39, false),
+                        new point(80,84, false),
+                        new point(101,12, false),
+                    }
+            },
+
+                new level()
+                {
+                    lvl = 1,
+                    pointsToMatch = new point[]
+                    {
+                        new point(15,10),
+                        new point(38,29, false),
+                        new point(36,14, false),
+                        new point(93,88, false),
+                        new point(98,22, false),
+                        new point(9,64, false),
+
+                        new point(106,85, false),
+                        new point(76,17, false),
+                        new point(106,85, false),
+                        new point(28,79, false),
+                        new point(28,44, false),
+                        new point(50,12, false),
+                        new point(75,39, false),
+                        new point(80,84, false),
+                        new point(101,12, false),
+                    }
+                }
+            };
+
+            r.reset_points = new point[]
+            {
+                new point(12,34),
+                new point(150,40),
+
+                new point(0,0, false),
+                new point(0,59, false),
+                new point(159,0, false),
+                new point(159,59, false),
+                new point(159,29, false),
+                new point(0,29, false),
+                new point(79,0, false),
+                new point(39,0, false),
+                new point(129,0, false),
+            };
+
+            return r;
+        }
+    }
+}
--- BO1ZombiesAutosplitter/Program.cs
+++ BO1ZombiesAutosplitter/Program.cs
@@ -21,7 +21,8 @@ namespace BO1ZombiesAutosplitter
     /// I have only tested this on resolution 1600x900, this is probably not going to
     /// work on any other resolutions atm
     /// 
-    /// I have tested round 1 up to 70 on Kino
+    /// TESTED SCENARIOS:
+    /// Kino der toten 1-70
     /// </summary>
     class Program
     {
@@ -31,17 +32,22 @@ namespace BO1ZombiesAutosplitter
         static readonly string BO1ProcessName = "BlackOps";
 
         static Process BO1Process;
-
         static Thread levelReadThread;
 
-        static List<level> Levels = new List<level>();
+        static resolution[] resolutions;
+        static resolution selectedResolution;
 
         static void Main(string[] args)
         {
             if (!GetProcess())
                 ExitProgram();
 
-            InitLevelPoints();
+            Points.InitializePoints(ref resolutions);
+
+            if (!SelectResolution())
+                Log("Your resolution is not supported", LogType.INFO);
+            else
+                Log("Found fitting resolution: " + selectedResolution.window_width + "x" + selectedResolution.window_height, LogType.INFO);
 
             levelReadThread = new Thread(new ThreadStart(ReadLevel));
             levelReadThread.Start();
@@ -52,281 +58,24 @@ namespace BO1ZombiesAutosplitter
             Console.Read();
         }
 
-        static void InitLevelPoints()
+        static bool SelectResolution()
         {
-            Levels.Add(
-                new level()
-                {
-                    lvl = 70,
-                    pointsToMatch = new point[]
-                    {
-                        // number seven
-                       new point(7,4),
-                       new point(20,4),
-                       new point(41,4),
-                       new point(40,11),
-                       new point(36,17),
-                       new point(29,29),
-                       new point(25,38),
-                       new point(21,47),
-                       new point(17,62),
-                       new point(16,77),
+            var rect = new User32.Rect();
+            User32.GetWindowRect(BO1Process.MainWindowHandle, ref rect);
 
-                         // number zero
-                        new point(64,42),
-                        new point(100,10),
-                        new point(84,2),
-                        new point(68,36),
-                        new point(103,31),
-                        new point(104,45),
-                        new point(97,56),
-                        new point(101,65),
-                        new point(94,74),
-                        new point(70,70),
+            int width = rect.right - rect.left;
+            int height = rect.bottom - rect.top;
+
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                if (resolutions[i].window_width == width && resolutions[i].window_height == height)
+                {
+                    selectedResolution = resolutions[i];
+                    return true;
                 }
-            });
+            }
 
-            Levels.Add(
-                new level()
-                {
-                    lvl = 50,
-                    pointsToMatch = new point[]
-                    {
-                        // number five
-                       new point(41,3),
-                       new point(10,4),
-                       new point(26,3),
-                       new point(8,13),
-                       new point(8,26),
-                       new point(8,36),
-                       new point(27,29),
-                       new point(37,34),
-                       new point(42,42),
-                       new point(43,53),
-                       new point(40,65),
-
-                       new point(34,72),
-                       new point(25,75),
-                       new point(11,72),
-                       new point(7,64),
-
-                        // number zero
-                        new point(64,42),
-                        new point(100,10),
-                        new point(84,2),
-                        new point(68,36),
-                        new point(103,31),
-                        new point(104,45),
-                        new point(97,56),
-                        new point(101,65),
-                        new point(94,74),
-                        new point(70,70),
-                }
-                });
-
-            Levels.Add(
-                new level()
-                {
-                    lvl = 30,
-                    pointsToMatch = new point[]
-                    {
-                        // number three
-                        new point(8,14),
-                        new point(14,3),
-                        new point(40,10),
-                        new point(37,28),
-                        new point(22,36),
-                        new point(37,42),
-                        new point(43,55),
-                        new point(40,69),
-                        new point(27,74),
-                        new point(12,73),
-                        new point(7,64),
-
-                        // number zero
-                        new point(64,42),
-                        new point(100,10),
-                        new point(84,2),
-                        new point(68,36),
-                        new point(103,31),
-                        new point(104,45),
-                        new point(97,56),
-                        new point(101,65),
-                        new point(94,74),
-                        new point(70,70),
-                    }
-                });
-
-            Levels.Add(
-                new level()
-                {
-                    lvl = 15,
-                    pointsToMatch = new point[]
-                    {
-                        // number one
-                        new point(30,2),
-                        new point(7,14),
-                        new point(28,76),
-
-                        // number five
-                        new point(100,3),                      
-                        new point(84,76),
-                        new point(103,53),
-                        new point(86,28),
-                        new point(68,36),
-                    }
-                });
-
-            Levels.Add(
-                new level()
-                {
-                lvl = 10,
-                pointsToMatch = new point[]
-                {
-                        new point(98,22),
-                        new point(9,64),
-                        new point(145,60),
-                        new point(169,51),
-                        new point(192,41),
-                        new point(225,22),
-                        new point(135,64),
-
-                        new point(106,85, false),
-                        new point(76,17, false),
-                        new point(106,85, false),
-                        new point(28,79, false),
-                        new point(28,44, false),
-                        new point(50,12, false),
-                        new point(75,39, false),
-                        new point(80,84, false),
-                        new point(101,12, false),
-                        new point(237,62, false),
-                    }
-                });
-
-            Levels.Add(
-                new level()
-                {
-                    lvl = 5,
-                    pointsToMatch = new point[]
-                    {
-                        new point(98,22),
-                        new point(9,64),
-                        new point(145,60, false),
-                        new point(169,51, false),
-                        new point(192,41, false),
-
-                        new point(106,85, false),
-                        new point(76,17, false),
-                        new point(106,85, false),
-                        new point(28,79, false),
-                        new point(28,44, false),
-                        new point(50,12, false),
-                        new point(75,39, false),
-                        new point(80,84, false),
-                        new point(101,12, false),
-                    }
-                });
-
-
-            Levels.Add(
-              new level()
-              {
-                  lvl = 4,
-                  pointsToMatch = new point[]
-                  {
-                        new point(90,19),
-                        new point(93,87),
-                        new point(98,22, false),
-                        new point(9,64, false),
-
-                        new point(106,85, false),
-                        new point(76,17, false),
-                        new point(106,85, false),
-                        new point(28,79, false),
-                        new point(28,44, false),
-                        new point(50,12, false),
-                        new point(75,39, false),
-                        new point(80,84, false),
-                        new point(101,12, false),
-                  }
-              });
-
-           Levels.Add(
-               new level()
-               {
-                   lvl = 3,
-                   pointsToMatch = new point[]
-                   {
-                       new point(15,12),
-                       new point(37,15),
-                       new point(63,16),
-                       new point(93,88, false),
-                       new point(98,22, false),
-                       new point(9,64, false),
-
-                       new point(106,85, false),
-                       new point(76,17, false),
-                       new point(106,85, false),
-                       new point(28,79, false),
-                       new point(28,44, false),
-                       new point(50,12, false),
-                       new point(75,39, false),
-                       new point(80,84, false),
-                       new point(101,12, false),
-                   }
-               });
-
-            Levels.Add(
-                new level()
-                {
-                    lvl = 2,
-                    pointsToMatch = new point[]
-                    {
-                        new point(14,13),
-                        new point(36,14),
-                        new point(38,29),
-                        new point(63,16, false),
-                        new point(93,88, false),
-                        new point(98,22, false),
-                        new point(9,64, false),
-
-                        new point(106,85, false),
-                        new point(76,17, false),
-                        new point(106,85, false),
-                        new point(28,79, false),
-                        new point(28,44, false),
-                        new point(50,12, false),
-                        new point(75,39, false),
-                        new point(80,84, false),
-                        new point(101,12, false),
-                    }
-                });
-
-            Levels.Add(
-                new level()
-                {
-                    lvl = 1,
-                    pointsToMatch = new point[] 
-                    {
-                        new point(15,10),
-                        new point(38,29, false),
-                        new point(36,14, false),
-                        new point(93,88, false),
-                        new point(98,22, false),
-                        new point(9,64, false),
-
-                        new point(106,85, false),
-                        new point(76,17, false),
-                        new point(106,85, false),
-                        new point(28,79, false),
-                        new point(28,44, false),
-                        new point(50,12, false),
-                        new point(75,39, false),
-                        new point(80,84, false),
-                        new point(101,12, false),
-                    }
-                });
+            return false;
         }
 
         static int currentLevel = 0;
@@ -379,36 +128,25 @@ namespace BO1ZombiesAutosplitter
         {
             Color clr = Color.FromArgb(214, 214, 214);
 
-            var px1 = bmp.GetPixel(12, 34);
-            var px2 = bmp.GetPixel(150, 40);
-
-            var px3 = bmp.GetPixel(0, 0);
-            var px4 = bmp.GetPixel(0, 59);
-            var px5 = bmp.GetPixel(159, 0);
-            var px6 = bmp.GetPixel(159, 59);
-
-            var px7 = bmp.GetPixel(159, 29);
-            var px8 = bmp.GetPixel(0, 29);
-            var px9 = bmp.GetPixel(79, 0);
-            var px10 = bmp.GetPixel(39, 0);
-            var px11 = bmp.GetPixel(129, 0);
-
-            if (ColorsAreClose(px1, clr, 25) && ColorsAreClose(px2, clr, 25))
+            bool matches = true;
+            for (int i = 0; i < selectedResolution.reset_points.Length; i++)
             {
-                // dont reset on nuke :)
-                if (!ColorsAreClose(px3, clr, 25) &&
-                    !ColorsAreClose(px4, clr, 25) &&
-                    !ColorsAreClose(px5, clr, 25) &&
-                    !ColorsAreClose(px6, clr, 25) &&
-                    !ColorsAreClose(px7, clr, 25) &&
-                    !ColorsAreClose(px8, clr, 25) &&
-                    !ColorsAreClose(px9, clr, 25) &&
-                    !ColorsAreClose(px10, clr, 25) &&
-                    !ColorsAreClose(px11, clr, 25))
-                return true;
+                var point = selectedResolution.reset_points[i];
+                var pixel = bmp.GetPixel(point.X, point.Y);
+
+                if (ColorsAreClose(pixel, clr, 25))
+                {
+                    if (!point.ShouldMatch)
+                        matches = false;
+                }
+                else
+                {
+                    if (point.ShouldMatch)
+                        matches = false;
+                }
             }
 
-            return false;
+            return matches;
         }
 
         private static int ReadLevelFromBmp(Bitmap bmp)
@@ -417,12 +155,12 @@ namespace BO1ZombiesAutosplitter
             Color redColor = Color.FromArgb(174, 174, 174);
             //Color redColor = Color.FromArgb(54, 0, 0);
 
-            for (int l = 0; l < Levels.Count; l++)
+            for (int l = 0; l < selectedResolution.levels.Length; l++)
             {
                 bool matches = true;
-                for (int c = 0; c < Levels[l].pointsToMatch.Length; c++)
+                for (int c = 0; c < selectedResolution.levels[l].pointsToMatch.Length; c++)
                 {
-                    var point = Levels[l].pointsToMatch[c];
+                    var point = selectedResolution.levels[l].pointsToMatch[c];
                     var pixel = bmp.GetPixel(point.X, point.Y);
 
                     // change to 55 if not working correctly
@@ -441,7 +179,7 @@ namespace BO1ZombiesAutosplitter
 
                 if (matches)
                 {
-                    return Levels[l].lvl;
+                    return selectedResolution.levels[l].lvl;
                 }
             }
 
@@ -552,25 +290,34 @@ namespace BO1ZombiesAutosplitter
             [DllImport("user32.dll")]
             public static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
         }
+    }
 
-        struct level
+    public struct level
+    {
+        public int lvl;
+        public point[] pointsToMatch;
+    }
+
+    public struct point
+    {
+        public int X;
+        public int Y;
+        public bool ShouldMatch;
+
+        public point(int x, int y, bool shouldMatch = true)
         {
-            public int lvl;
-            public point[] pointsToMatch;
+            this.X = x;
+            this.Y = y;
+            this.ShouldMatch = shouldMatch;
         }
+    }
 
-        struct point
-        {
-            public int X;
-            public int Y;
-            public bool ShouldMatch;
+    public struct resolution
+    {
+        public level[] levels;
+        public point[] reset_points;
 
-            public point(int x, int y, bool shouldMatch = true)
-            {
-                this.X = x;
-                this.Y = y;
-                this.ShouldMatch = shouldMatch;
-            }
-        }
+        public int window_width;
+        public int window_height;
     }
 }
